a3a025fc3f4367499984f3ea38a9dadb663dda0d
Evan Driscoll <driscoll@cs.wisc.edu>
2012-11-10 00:11:34 -0600

    Delete some actual drawing code from buchheim.py

--- buchheim.py
+++ buchheim.py
@@ -157,27 +157,3 @@ def second_walk(v, m=0, depth=0, min=None):
 
     return min
 
-r = 30
-rh = r*1.5
-rw = r*1.5
-stroke(0)
-
-def drawt(root, depth):
-    global r
-    oval(root.x * rw, depth * rh, r, r)
-    print root.x
-    for child in root.children:
-        drawt(child, depth+1)
-
-def drawconn(root, depth):
-    for child in root.children:
-        line(root.x * rw + (r/2), depth * rh + (r/2),
-             child.x * rw + (r/2), (depth+1) * rh + (r/2))
-        drawconn(child, depth+1)
-
-size(1000, 500)
-translate(2, 2)
-stroke(0)
-drawconn(dt, 0)
-fill(1,1,1)
-drawt(dt, 0)
