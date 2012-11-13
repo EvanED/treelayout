9afdf6ee5298a0762a9c752144a40dec320d6993
Evan Driscoll <driscoll@cs.wisc.edu>
2012-11-13 11:12:06 -0600

    setup.py file that seems to install things

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name="treelayout",
+      description="Family of tree layout algorithms",
+      version="0.1alpha",
+      author="Bill Mill",
+      maintainer="Evan Driscoll",
+      maintainer_email="driscoll@cs.wisc.edu",
+      #url="http://
+      packages=["treelayout"],
+      package_dir = {'': 'src'},
+      license = "WTFPL",
+      )
+
